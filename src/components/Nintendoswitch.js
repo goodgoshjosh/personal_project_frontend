728f4cd89da792ebdab91d5de81b8cbecf010225
Josh Lee <joshuadavidis@gmail.com>
2019-01-08 13:58:16 -0700

    Created a Nintendoswitch.css file and added some experimental code in the .js file but commented it out

--- src/components/Nintendoswitch.js
+++ src/components/Nintendoswitch.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import ".Nintendoswitch.css";
 
 class Nintendoswitch extends Component {
-    render() 
+    render()
+//    { return (
+//        /* <img 
+//         src="https://www.logolynx.com/images/logolynx/74/74b158460c56301f7ae19c9b18109ccd.png"
+//         alt="Nintendo Switch Logo"
+//         /> 
+//     )}; 
 }
 
 export default Nintendoswitch;
