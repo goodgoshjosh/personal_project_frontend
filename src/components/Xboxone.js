484a8d5972de4d8f242626f2390f3592e584a18d
Josh Lee <joshuadavidis@gmail.com>
2019-01-11 15:01:36 -0700

    Copied the data so that now Xbox, Nintendo Switch and PS4 all Fetch data from the database and map it onto the table

--- src/components/Xboxone.js
+++ src/components/Xboxone.js
@@ -1,16 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import './Xboxone.css'
 import { apiURL } from '../App'
 
 class Xboxone extends Component {
-    render() {
+  constructor() {
+    super();
+    this.state = { xboxData: [] }
+    }
+
+    componentDidMount() {
+      
+      fetch(`${apiURL}/xbox_ones`, {
+        headers: {
+          "Content-Type":"application/json"
+        }
+      })
+      .then(response => response.json() )
+      .then(data => this.setState({xboxData: data}))
+    }
+  
+
+  render() {
+
+    let xboxData = this.state.xboxData
+
         return (
         <div className="Xboxone">
             <h1>Xbox One Game Collection</h1>
             <img class='xboxlogo' src='https://media.ctrl.blog/1b/5d/xbox_logo_1024/300x300.png' alt='Xbox Logo' />
        
-            <table class="table table-hover">
-                <thead>
+   
+   
+   <table class="table table-hover">
+    <thead>
     <tr>
       <th scope="col">Title</th>
       <th scope="col">Genre</th>
@@ -21,22 +43,28 @@ class Xboxone extends Component {
       <th scope="col">ID #</th>
     </tr>
   </thead>
+  
+  
   <tbody>
-    <tr class="table-success">
-      <td>Column Content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-  </tbody>
-</table> 
+      {xboxData.map(item =>
+      <Fragment key={item.id}>
 
-        </div>
-        )
+         <tr class="table-success">
+          <td>{item.name}</td>
+          <td>{item.genre}</td>
+          <td>{item.description}</td>
+          <td>{item.rating}</td>
+          <td>{item.digital_copy}</td>
+          <td>{item.physical_copy}</td>
+          <td>{item.id}</td>
+        </tr>
+        </Fragment> //Fragments don't take up as much dedicated space as a div. This is a parent element that doesn't take up space
+      )}
+      </tbody>
+    </table> 
+    </div>
+       )
     }
-}
+  }
 
 export default Xboxone;
