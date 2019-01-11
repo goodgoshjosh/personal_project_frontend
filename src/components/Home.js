995e312984cfa627a423e0df87862a3ace04d9f9
Josh Lee <joshuadavidis@gmail.com>
2019-01-11 15:42:10 -0700

    Added a drop down menu for eventual form

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,21 @@ class Home extends Component {
             <h1>Welcome home!</h1>
             <p> This full-stack application uses PostGreSQL and Elephant SQL to make a database that connects to Loopback as its API that then uses NOW to deploy online, that the application fetches and maps onto tables.</p>
             <p>In each system (Nintendo Switch, PS4, and Xbox One), you will see a table that corresponds to this database.</p>
+        
+
+        <p>Below will be where you update data via a form and submit through the selector</p>
+
+        <p>FORM GOES HERE</p>
+
+            <div class="form-group">
+                <select class="custom-select">
+                    <option selected="">Console Selector</option>
+                    <option value="1">Nintendo Switch</option>
+                    <option value="2">PS4</option>
+                    <option value="3">Xbox One</option>
+                </select>
+             </div>
+        
         </div>        
         )
     }
