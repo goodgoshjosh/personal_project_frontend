2968305189210526760546636f4b574f3d78a5b9
Josh Lee <joshuadavidis@gmail.com>
2019-01-14 09:27:06 -0700

    Made some additional notes for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,8 @@ class Home extends Component {
         <div className="Home">
             <h1>Welcome home!</h1>
             <p> This full-stack application uses PostGreSQL and Elephant SQL to make a database that connects to Loopback as its API that then uses NOW to deploy online, that the application fetches and maps onto tables.</p>
-            <p>In each system (Nintendo Switch, PS4, and Xbox One), you will see a table that corresponds to this database.</p>
+            <p> In each system (Nintendo Switch, PS4, and Xbox One), you will see a table that corresponds to this database.</p>
+            <p> This site is a work-in-progress. The first time you load it, it may take a few moments for the database to load as NOW's hosting takes a moment to get going.</p>
         
 
         <p>Below will be where you update data via a form and submit through the selector</p>
